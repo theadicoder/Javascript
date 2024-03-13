7a43ca301267ca5cea7e8e926b9936c214825db7
ADITYA THAKUR <Aadicorian@outlook.com>
2024-03-13 10:40:36 +0000

    updated

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 // Hello World : 
 console.log("Hello World")  
 console.log('Aditya Thakur')
-console.log('I Love Apple I - Phone')
+console.log('I Love Apple I - Phone')//basic understanding how we write small line code in js
 
 // Variable : 
 
-Naturalname = 'theadicoder';
-age = 16;
-price =' 100 $ ';
-X = null;
-Y = undefined;
-radius = 14;
-console.log(Naturalname,age,price,X,Y);
+Naturalname = 'theadicoder';//simple string
+age = 16;//Integer
+price =' 100 $ ';//string var
+X = null;//null
+Y = undefined;// undefined
+radius = 14;// Integer
+isFollow = true;// Boolean
+console.log(Naturalname,age,price,X,Y);//run all over code here
+
+// Javascript Was Dynamically Typed Language which mean we can change the one or two line code value on run time and then we can't do it on another langugaes .
+
+// we can't write any variables name staring from a no. because for its an integer but also we can write it in string and alphabatic form.
+
+// eg - we can writing staring with  _, string, a-b..
+
+// eg - what we can't do in any 
