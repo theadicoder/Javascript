b96b7203b51044cfede05cfb454e768b0f3f5c9a
ADITYA THAKUR <Aadicorian@outlook.com>
2024-03-13 10:29:00 +0000

    Updated

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hello World : 
+console.log("Hello World")  
+console.log('Aditya Thakur')
+console.log('I Love Apple I - Phone')
+
+// Variable : 
+
+Naturalname = 'theadicoder';
+age = 16;
+price =' 100 $ ';
+X = null;
+Y = undefined;
+radius = 14;
+console.log(Naturalname,age,price,X,Y);
